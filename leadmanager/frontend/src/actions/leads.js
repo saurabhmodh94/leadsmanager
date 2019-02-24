1dec6bf5ebedf7100c79de2a509a0a32ba01622a
Saurabh Modh <saurabhmodh94@gmail.com>
2019-02-24 23:42:05 +0530

    alert success messages

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+import { createMessage } from "./messages";
 
 // GET LEADS
 export const getLeads = () => dispatch => {
@@ -22,6 +23,7 @@ export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
     .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({
         type: DELETE_LEAD,
         payload: id
@@ -35,6 +37,7 @@ export const addLead = lead => dispatch => {
   axios
     .post("/api/leads/", lead)
     .then(res => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({
         type: ADD_LEAD,
         payload: res.data
