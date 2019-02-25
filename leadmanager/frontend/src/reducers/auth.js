87496110a0d0b3251db1a34fe8d960723d4dffbc
Saurabh Modh <saurabhmodh94@gmail.com>
2019-02-25 16:22:49 +0530

    init auth reducer

---
+++ leadmanager/frontend/src/reducers/auth.js
@@ -0,0 +1,17 @@
+
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  isLoading: false,
+  user: null
+};
+
+export default function (state = initialState, action) {
+  switch (action.type)
+  {
+
+    default:
+      return state;
+  }
+}
