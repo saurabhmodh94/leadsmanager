65738cca1917bdadcf6d2822a5c6cbb31cb409ed
Saurabh Modh <saurabhmodh94@gmail.com>
2019-02-25 17:16:54 +0530

    Navbar Links based on isAuth

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 export class Header extends Component {
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <span className="navbar-text mr-3">
+          <strong>{user ? `Welcome ${user.username}` : ""}</strong>
+        </span>
+        <li className="nav-item">
+          <button
+            className="nav-link btn btn-info btn-sm text-light"
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
     return (
       <div>
         <nav className="navbar navbar-expand-sm navbar-light bg-light">
@@ -12,22 +45,21 @@ export class Header extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
           </div>
-          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li className="nav-item">
-              <Link to="/register" className="nav-link">
-                Register
-          </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">
-                Login
-          </Link>
-            </li>
-          </ul>
+          {isAuthenticated ? authLinks : guestLinks}
         </nav>
       </div>
     )
   }
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  };
 }
 
-export default Header
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(Header);
