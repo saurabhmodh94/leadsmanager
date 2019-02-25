5ec86302b214c3264eda317cc3773c9784ded392
Saurabh Modh <saurabhmodh94@gmail.com>
2019-02-25 16:55:51 +0530

    loadUser api call from frontend

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -16,6 +16,7 @@ import PrivateRoute from "./common/PrivateRoute";
 
 import { Provider } from "react-redux";
 import store from "../store";
+import { loadUser } from "../actions/auth";
 
 import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
@@ -27,7 +28,9 @@ const alertOptions = {
 };
 
 class App extends Component {
-
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
   render() {
     return (
       <Provider store={store}>
