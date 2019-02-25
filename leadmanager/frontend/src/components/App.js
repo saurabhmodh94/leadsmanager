9076a2ab72c64d1ed448cf49059e39bd9f5747c6
Saurabh Modh <saurabhmodh94@gmail.com>
2019-02-25 16:23:11 +0530

    PrivateRoute Config

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -12,6 +12,7 @@ import Header from "./layout/Header";
 import Alerts from "./layout/Alerts";
 import Login from "./accounts/Login";
 import Register from "./accounts/Register";
+import PrivateRoute from "./common/PrivateRoute";
 
 import { Provider } from "react-redux";
 import store from "../store";
@@ -37,7 +38,7 @@ class App extends Component {
                 <Header></Header>
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Dashboard} />
+                  <PrivateRoute exact path="/" component={Dashboard} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
                 </Switch>
