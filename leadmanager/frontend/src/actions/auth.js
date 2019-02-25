f4e2b196909d25e7ac540dac2a9fafc5a8a44e83
Saurabh Modh <saurabhmodh94@gmail.com>
2019-02-25 17:09:31 +0530

    login API call from frontend

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -4,7 +4,9 @@ import { returnErrors } from "./messages";
 import {
   USER_LOADED,
   USER_LOADING,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from "./types";
 
 // CHECK TOKEN & LOAD USER
@@ -47,4 +49,32 @@ export const tokenConfig = getState => {
   }
 
   return config;
+};
+
+// LOGIN USER
+export const login = (username, password) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // Request Body
+  const body = JSON.stringify({ username, password });
+
+  axios
+    .post("/api/auth/login", body, config)
+    .then(res => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
 };
